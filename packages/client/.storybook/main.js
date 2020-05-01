9c14e8b0c308943565cb18c0a9241141781ae12b
Kim Yong <rladyd818@live.co.kr>
2020-05-01 16:56:27 +0900

    scss 문제

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -1,14 +1,39 @@
+const aliasImporter = require("node-sass-alias-importer");
+
+const path = require("path");
 module.exports = {
 	stories: ["../src/**/*.stories.[tj]sx"],
 	webpackFinal: async config => {
-		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
-			loader: require.resolve("babel-loader"),
-			options: {
-				presets: [["react-app", { flow: false, typescript: true }]]
-			}
-		});
+		config.module.rules.push(
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							// Prefer `dart-sass`
+							implementation: require("sass"),
+							importer: aliasImporter({
+								styles: "../styles",
+							}),
+						},
+					},
+				],
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				loader: require.resolve("babel-loader"),
+				options: {
+					presets: [["react-app", { flow: false, typescript: true }]],
+				},
+			},
+		);
 		config.resolve.extensions.push(".ts", ".tsx");
+		config.resolve.alias = {
+			styles: path.join(__dirname, "../styles"),
+		};
 		return config;
-	}
+	},
 };
